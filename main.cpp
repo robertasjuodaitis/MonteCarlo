5f5a50baff9b884e9f7bd89549719035ca253ae2
robertasjuodaitis <r.juodaitis@gmail.com>
2012-10-09 18:38:57 +0200

    Pridejau xSprend

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main()
 {
     cout << "Monte Carlo metodo realizacijos pradzia" << endl;
+    cout << "(RANDOM SEARCH METHOD)" << endl;
     double f, x; //fjos r ir arg x
     int n; //dimensija
     int a,b,c; //la la
+    double xSprend; // Arg
     return 0;
 }

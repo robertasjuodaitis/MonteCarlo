69248931a711c3d8fed0616b0d0795ef6aa8904b
robertasjuodaitis <r.juodaitis@gmail.com>
2012-10-09 18:34:43 +0200

    Papildziau kintamuju aprasyma

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Monte Carlo metodo realizacijos pradzia" << endl;
+    double f, x; //fjos r ir arg x
+    int n; //dimensija
+    int a,b,c; //la la
     return 0;
 }
